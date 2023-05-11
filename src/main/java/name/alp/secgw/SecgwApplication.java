ddeed60441a58a687e0ae213c37cf4feb9d90ea1
Alphan Arslan <alphan.arslan@softtech.com.tr>
2023-05-11 10:07:52 +0300

    initial commit

---
+++ src/main/java/name/alp/secgw/SecgwApplication.java
@@ -0,0 +1,13 @@
+package name.alp.secgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecgwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecgwApplication.class, args);
+	}
+
+}
